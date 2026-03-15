b4370eddb3959419a417cde0f7479972ba82ae7c
harshamubt-debug <harsha.mubt@gmail.com>
2026-03-15 10:01:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from Bio import Entrez
+import pandas as pd
+import plotly.express as px
 
 Entrez.email = "your_email@gmail.com"
 
@@ -27,3 +29,41 @@ if disease:
     st.write("NCBI:", f"https://www.ncbi.nlm.nih.gov/search/all/?term={disease}")
     st.write("UniProt:", f"https://www.uniprot.org/uniprotkb?query={disease}")
     st.write("WHO Data:", "https://www.who.int/data")
+
+# ----------- GLOBAL DATA VISUALIZATION -----------
+
+st.subheader("Global Disease Data")
+
+url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
+data = pd.read_csv(url)
+
+# ----------- CASES OVER TIME GRAPH -----------
+
+st.subheader("Cases Over Time (India Example)")
+
+india_data = data[data["location"] == "India"]
+
+fig = px.line(
+    india_data,
+    x="date",
+    y="total_cases",
+    title="Total Disease Cases Over Time"
+)
+
+st.plotly_chart(fig)
+
+# ----------- WORLD MAP -----------
+
+st.subheader("Global Disease Distribution")
+
+latest = data.sort_values("date").groupby("location").tail(1)
+
+fig_map = px.choropleth(
+    latest,
+    locations="iso_code",
+    color="total_cases",
+    hover_name="location",
+    title="Global Disease Cases"
+)
+
+st.plotly_chart(fig_map)
